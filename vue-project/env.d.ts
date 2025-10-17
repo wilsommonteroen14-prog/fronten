c357f7b55c0e3a857c689627fd76be60da070c5a
Wilson Montero <wilsommonteroen14@gmail.com>
2025-10-17 09:46:29 -0400

    Fix: Hacer opcional el campo de dirección en la API

--- vue-project/env.d.ts
+++ vue-project/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+// --- ESTAS SON LAS LÍNEAS CLAVE ---
+// Le dices a TypeScript que cualquier archivo que termine en .vue
+// es un componente de Vue válido y defines su tipo.
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
